17cb445cac8e1a895f01c6c4c9e9e9bb104aebf3
UcedaMunto <alex.uceda@hotmail.com>
2024-10-21 21:24:23 -0600

    imagenes y otros patronos

--- codes/bridge/bridge.py
+++ codes/bridge/bridge.py
@@ -2,39 +2,41 @@ import abc
 from abc import ABC, abstractmethod
 # https://refactoring.guru/es/design-patterns/bridge
 
+# ________________ INTERFAZ
 class IFormat(ABC):
     @abstractmethod
     def play(self, file_path: str) -> None:
         pass
-
+# ________________ CLASE ABSTRACTA
 class MusicPlayer( ABC ):
-    def __init__(self, format:IFormat):
+    def __init__(self, format:IFormat): # IFormat se utiliza como un contrato que permite conectar las clases  
         self.format = format 
-
     @abstractmethod
     def play(self, file_path: str) -> None:
         pass
+#__________________ORIGEN
+class MP3(IFormat): 
+    print(f"MP3 Implementa la interfaz IFormat")
+    def play(self, file_path: str) -> None:
+        print(f"PLaying WAV FILE {file_path}")
+class WAV(IFormat):
+    print(f"WAV Implementa la interfaz IFormat")
+    def play(self, file_path: str) -> None:
+        print(f"PLaying WAV FILE {file_path}")
 
+#__________________DESTINO
 class DesktopPlayer(MusicPlayer):
     def play(self, file_path:str) -> None:
         print("Using Mobile player")
         self.format.play( file_path )
-
 class MobilPlayer(MusicPlayer):
     def play(self, file_path:str) -> None:
         print("Using Mobile player")
         self.format.play( file_path )
 
-class MP3(IFormat):
-    def play(self, file_path: str) -> None:
-        print(f"PLaying WAV FILE {file_path}")
 
+#__________________EJECUCION
 
-class WAV(IFormat):
-    def play(self, file_path: str) -> None:
-        print(f"PLaying WAV FILE {file_path}")
-
-    
 mp3_format = MP3()
 player = DesktopPlayer(mp3_format)
 player.play("song mp3")
